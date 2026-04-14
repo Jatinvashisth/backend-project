6979a8227ba5305570d06dac5a956adfb3061927
jatin vashisth <jatinvashisth91@gmail.com>
2026-04-14 15:21:24 +0530

    fixed

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const app = express();
 
-// Root endpoint
 app.get("/", (req, res) => {
+  console.log("root endpoint invoked");
   res.send("Hi DevOps");
 });
 
-// Health check endpoint
 app.get("/check", (req, res) => {
+  console.log("health check endpoint invoked");
   res.json({ status: "OK" });
 });
 
-// Server start
 app.listen(3000,()=> {
   console.log("Server running on port 3000");
 });
