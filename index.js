60aac62c64802d5e9d576a491264c975ad8c3036
jatin vashisth <jatinvashisth91@gmail.com>
2026-04-21 22:28:26 +0530

    changes in index.js file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   console.log("root endpoint invoked");
-  res.send(`<html><body style="margin:0;height:100vh;display:flex;justify-content:center;align-items:center;background:linear-gradient(135deg,#ff4e50,#1fddff);"><h1 style="color:white;font-size:60px;text-shadow:2px 2px 10px rgba(0,0,0,0.4);animation:glow 1.5s infinite alternate;">Hi DevOps 🚀</h1><style>@keyframes glow{from{transform:scale(1);}to{transform:scale(1.1);}}</style></body></html>`); });
+  res.send(`<html><body style="margin:0;height:100vh;display:flex;justify-content:center;align-items:center;background:linear-gradient(135deg,#ff4e50,#1fddff);"><h1 style="color:white;font-size:60px;text-shadow:2px 2px 10px rgba(0,0,0,0.4);animation:glow 1.5s infinite alternate;">Hi DevOps !! 🚀</h1><style>@keyframes glow{from{transform:scale(1);}to{transform:scale(1.1);}}</style></body></html>`); });
 
 
 app.get("/check", (req, res) => {
